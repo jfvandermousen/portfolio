b94d38d750c69a2e45d7d4a09bc8559b1530209c
jfvandermousen <jeanfrancoisvandermousen@gmail.com>
2021-09-08 07:56:43 +0200

    styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,54 +1,12 @@
-import React, { useState, useEffect, useRef } from "react";
-import { withRouter, Link, useHistory } from "react-router-dom";
+import React, {  useEffect,} from "react";
+// import { withRouter, Link, useHistory } from "react-router-dom";
 import gsap from "gsap"
-import Menu from './Menu';
+// import Menu from './Menu';
 import logo from '../img/logo.png';
 
 
 export default function Header() {
-    const history = useHistory()
-
-
-    //State of our Menu
-  const [state, setState] = useState({
-    initial: false,
-    clicked: null,
-    menuName: "Menu",
-  });
-
-// State of our button
-const [disabled, setDisabled] = useState(false);
-
-//toggle menu
-const toggleMenu = () => {
-    disableMenu();
-    if (state.initial === false) {
-      setState({
-        initial: null,
-        clicked: true,
-        menuName: "Close",
-      });
-    } else if (state.clicked === true) {
-      setState({
-        clicked: !state.clicked,
-        menuName: "Menu",
-      });
-    } else if (state.clicked === false) {
-      setState({
-        clicked: !state.clicked,
-        menuName: "Close",
-      });
-    }
-  };
-  // check if out button is disabled
-  const disableMenu = () => {
-    setDisabled(!disabled);
-    setTimeout(() => {
-      setDisabled(false);
-    }, 1200);
-  };
-
-  // translate logo
+        
         useEffect(()=>{
             // let y = window.screen.width;
             // var negY = ( -y ); // => -100
