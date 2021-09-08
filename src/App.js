2017c5dc8f63997c278e882e48b940a1a07c192c
jfvandermousen <jeanfrancoisvandermousen@gmail.com>
2021-09-08 10:12:51 +0200

    add top padding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import About from "./components/About";
 import Header from "./components/Header";
+import Works from "./components/Works";
 
 
 
 function App() {
   return (
-    <div className="container mx-auto px-8">
+    <div className="container mx-auto px-8 pt-8">
       <Header/>
       <About />
     </div>
