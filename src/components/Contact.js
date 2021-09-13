065fb236d10c56e8883459e9f40a354df0839db9
jfvandermousen <jeanfrancoisvandermousen@gmail.com>
2021-09-13 16:39:15 +0200

    style contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,37 +1,47 @@
 import React, { useState } from "react";
 
 export default function Contact(props) {
-  const [name, setName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
   
   const handleSubmit = (evt) => {
       evt.preventDefault();
-      alert(`Submitting Name ${name}`)
+      alert(`${fullName}`)
   }
   return (
 
     <div>
-        <p>You can reach me by :</p>
-    <form className="flex flex-col" onSubmit={handleSubmit}>
-      <label>
-        First Name:
-        <input
-          type="text"
-          value={name}
-          onChange={e => setLastName(e.target.value)}
-        />
-      </label>
-      <label>
-        Last Name:
-        <input
-          type="text"
-          value={lastName}
-          onChange={e => setLastName(e.target.value)}
-        />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+      <p>You can reach me by :</p>
+      <form classNameName="flex flex-col" onSubmit={handleSubmit}>
+          <div className="mt-4 mr-2">
+            <span className="uppercase text-sm form-lab font-bold">Full Name</span>
+            <input className="w-full bg-gray-100 form-input mt-2 p-3  focus:outline-none focus:shadow-outline"
+              type="text"
+              value={fullName}
+              onChange={e => setFullName(e.target.value)}/>
+            </div>
 
+          <div className="mt-8 mr-2">
+            <span className="uppercase text-sm form-lab font-bold">Email</span>
+            <input className="w-full bg-gray-100 form-input mt-2  p-3  focus:outline-none focus:shadow-outline"
+              type="text"
+              value={email}
+              onChange={e => setEmail(e.target.value)}/>
+          </div>
+         
+          <div className="mt-8 mr-2">
+            <span className="uppercase text-sm form-lab font-bold">Message</span>
+            <textarea className="w-full bg-gray-100 form-input mt-2  p-3  focus:outline-none focus:shadow-outline"
+              type="text"
+              value={message}
+              onChange={e => setMessage(e.target.value)}/>
+          </div>
+          <button className="w-full mt-8 bg-btn  text-white font-bold py-2 px-4 "
+          type="submit">
+  SEND
+</button>
+    </form>
     </div>
   );
 }
