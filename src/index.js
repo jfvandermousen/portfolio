14696a4cc5e8d1bf3cd3da198213867d726b7fa7
jfvandermousen <jeanfrancoisvandermousen@gmail.com>
2021-09-20 10:24:49 +0200

    remove unused import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import {  Provider as AlertProvider } from 'react-alert'
 import AlertTemplate from 'react-alert-template-basic'
 
 
