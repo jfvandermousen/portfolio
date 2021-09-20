ffb6da1789d802e56eb00cebdfca39002fd5615e
jfvandermousen <jeanfrancoisvandermousen@gmail.com>
2021-09-20 12:12:06 +0200

    change size title works

--- src/components/HOMEPAGE/Works.js
+++ src/components/HOMEPAGE/Works.js
@@ -30,7 +30,7 @@ export default function Works() {
                     src={project.image}
                   />
                   <div className=" project-h  px-4 py-5 relative z-10 project-h border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                    <h2 className="tracking-widest text-lg title-font font-medium text-green-400 mb-1">
+                    <h2 className="tracking-widest text-xl title-font font-medium text-green-400 mb-1">
                       {project.subtitle}
                     </h2>
                     <h1 className="title-font text-2xl font-medium text-white mb-3">
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -28,7 +28,7 @@ export default function Projects() {
                 src={project.image}
               />
               <div className="px-4 py-10  project-h relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                <h2 className="tracking-widest text-lg title-font font-medium text-green-400 mb-1">
+                <h2 className="tracking-widest text-xl title-font font-medium text-green-400 mb-1">
                   {project.subtitle}
                 </h2>
                 <h1 className="title-font text-2xlfont-medium text-white mb-3">
